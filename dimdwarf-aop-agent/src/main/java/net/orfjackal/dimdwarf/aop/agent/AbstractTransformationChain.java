471997f3643482d63999e54dd651d2880b94f3dd
Esko Luontola <esko.luontola@gmail.com>
2008-11-19 23:23:14 +0000

    TODO

--- dimdwarf-aop-agent/src/main/java/net/orfjackal/dimdwarf/aop/agent/AbstractTransformationChain.java
+++ dimdwarf-aop-agent/src/main/java/net/orfjackal/dimdwarf/aop/agent/AbstractTransformationChain.java
@@ -31,12 +31,9 @@
 
 package net.orfjackal.dimdwarf.aop.agent;
 
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.*;
 
-import java.lang.instrument.ClassFileTransformer;
-import java.lang.instrument.IllegalClassFormatException;
+import java.lang.instrument.*;
 import java.security.ProtectionDomain;
 
 /**
@@ -47,6 +44,7 @@ public abstract class AbstractTransformationChain implements ClassFileTransforme
 
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
                             ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+        // TODO: at least the ClassLoader could be passed to the adapters, so they could examine super classes, package annotations etc. 
         ClassReader cr = new ClassReader(classfileBuffer);
         ClassWriter cw;
         if (enableAdditiveTransformationOptimization()) {
