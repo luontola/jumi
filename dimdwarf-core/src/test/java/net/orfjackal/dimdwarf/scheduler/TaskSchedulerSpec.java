84f91d1968f56c9937326db21bde076cc7ed704e
Esko Luontola <esko.luontola@gmail.com>
2008-11-28 00:46:57 +0000

    TODO

--- dimdwarf-core/src/test/java/net/orfjackal/dimdwarf/scheduler/TaskSchedulerSpec.java
+++ dimdwarf-core/src/test/java/net/orfjackal/dimdwarf/scheduler/TaskSchedulerSpec.java
@@ -145,6 +145,7 @@ public class TaskSchedulerSpec extends Specification<Object> {
             public void run() throws Throwable {
                 TaskBootstrap bootstrap;
                 do {
+                    // XXX: Sometimes blocks here when stress testing. Maybe the interrupt disappears somewhere? (WhenATaskIsScheduledWithFixedDelay.theFollowingExecutionsAreAfterTheFixedDelay)
                     bootstrap = scheduler.takeNextTask();
                 } while (isNullTask(bootstrap));
             }
