db1b2808baf9096a4cbe85adf81f3c297c52dc91
Esko Luontola <esko.luontola@gmail.com>
2008-12-11 00:19:31 +0000

    TODO

--- dimdwarf-core/src/test/java/net/orfjackal/dimdwarf/gc/cms/ConcurrentMarkSweepCollectorIntegrationSpec.java
+++ dimdwarf-core/src/test/java/net/orfjackal/dimdwarf/gc/cms/ConcurrentMarkSweepCollectorIntegrationSpec.java
@@ -162,4 +162,6 @@ public class ConcurrentMarkSweepCollectorIntegrationSpec extends Specification<O
             specify(entityExists(garbageCycleId2), should.equal(false));
         }
     }
+
+    // TODO: MutatorListener
 }
