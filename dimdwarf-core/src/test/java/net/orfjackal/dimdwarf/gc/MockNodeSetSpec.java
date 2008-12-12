e33c55b759db27470a94938bcbb1b2753ae3db22
Esko Luontola <esko.luontola@gmail.com>
2008-12-12 11:30:51 +0000

    NodeSet

---
+++ dimdwarf-core/src/test/java/net/orfjackal/dimdwarf/gc/MockNodeSetSpec.java
@@ -0,0 +1,73 @@
+/*
+ * This file is part of Dimdwarf Application Server <http://dimdwarf.sourceforge.net/>
+ *
+ * Copyright (c) 2008, Esko Luontola. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *       this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names of its contributors
+ *       may be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.orfjackal.dimdwarf.gc;
+
+import jdave.*;
+import jdave.junit4.JDaveRunner;
+import org.junit.runner.RunWith;
+
+import java.util.*;
+
+/**
+ * @author Esko Luontola
+ * @since 12.12.2008
+ */
+@RunWith(JDaveRunner.class)
+@Group({"fast"})
+public class MockNodeSetSpec extends Specification<Object> {
+
+    private MockNodeSet set = new MockNodeSet();
+
+
+    public class WhenNodeSetIsEmpty {
+
+        public void noNodesCanBeTakeFromIt() {
+            specify(set.pollFirst(), should.equal(null));
+        }
+    }
+
+    public class WhenNodesHaveBeenAddedToNodeSet {
+
+        public void create() {
+            set.add("A");
+            set.add("B");
+        }
+
+        public void thoseNodesCanBeTakenFromIt() {
+            List<String> taken = new ArrayList<String>();
+            taken.add(set.pollFirst());
+            taken.add(set.pollFirst());
+            specify(taken, should.containExactly("A", "B"));
+            specify(set.pollFirst(), should.equal(null));
+        }
+    }
+}
