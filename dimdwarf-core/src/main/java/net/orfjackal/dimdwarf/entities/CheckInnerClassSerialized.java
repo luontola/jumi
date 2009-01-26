bedc4c223d50315bdfa18bc65eb342f2e51600e8
Esko Luontola <esko.luontola@gmail.com>
2009-01-26 02:42:32 +0200

    Improved the comment related to serializing local classes

--- dimdwarf-core/src/main/java/net/orfjackal/dimdwarf/entities/CheckInnerClassSerialized.java
+++ dimdwarf-core/src/main/java/net/orfjackal/dimdwarf/entities/CheckInnerClassSerialized.java
@@ -40,10 +40,12 @@ public class CheckInnerClassSerialized extends SerializationAdapter {
 
     @Override
     public void beforeSerialize(Object rootObject, Object obj, MetadataBuilder meta) {
-        // Serializing anonymous and local classes is dangerous, because their class names are
-        // generated automatically ($1, $2 etc.), which means that the next time that such an
+        // Serializing anonymous and local classes is dangerous, because their class names are generated
+        // automatically ($1, $2, $1Local, $2Local etc.), which means that the next time that such an
         // object is deserialized from database, the class name might have changed because of
         // a change to the enclosing class, and the system might end up in an unspecified state.
+        // Also "Java Object Serialization Specification 6.0" strongly discourages serialization of
+        // inner classes (http://java.sun.com/javase/6/docs/platform/serialization/spec/serial-arch.html#7182).
         // It is safer to serialize only top-level classes and member classes.
         Class<?> cls = obj.getClass();
         if (cls.isAnonymousClass()) {
