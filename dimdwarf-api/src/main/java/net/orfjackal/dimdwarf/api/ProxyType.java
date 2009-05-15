67e661c1d8e90c1359c857c584bccd1ca6a06832
Esko Luontola <esko.luontola@gmail.com>
2009-05-15 23:30:12 +0300

    Documented the benefits and bug traps of the alternative ProxyTypes.

--- dimdwarf-api/src/main/java/net/orfjackal/dimdwarf/api/ProxyType.java
+++ dimdwarf-api/src/main/java/net/orfjackal/dimdwarf/api/ProxyType.java
@@ -13,14 +13,29 @@ package net.orfjackal.dimdwarf.api;
 public enum ProxyType {
 
     /**
-     * The proxy will extend {@link Object} and implement the same interfaces as the target class.
+     * The proxy will extend {@link Object} and implement the same interfaces as the target class. This is the safest
+     * option, but requires that the class has interfaces and is always used through its interfaces. Otherwise an
+     * exception will happen at runtime, when some objects are deserialized or when trying to cast the proxy to the
+     * type of the target class.
      */
     INTERFACE,
 
     /**
-     * The proxy will extend the target class. The target class should not have any publicly accessible fields,
-     * because the proxy can not intercept field access. The constructor of the target class will not be called
-     * when the proxy is created.
+     * The proxy will extend the target class. This way the class does not need to be used through interfaces, but the
+     * programmer needs to be aware of some restrictions to avoid bug traps:
+     * <ul>
+     * <li>The target class should not have any publicly accessible fields, because the proxy can not intercept field
+     * access.</li>
+     * <li>Neither should the class access the private fields of another instance of the same class (unfortunately Java
+     * has only a class-private and not an instance-private scope), because it could very well be a proxy instead of
+     * the actual object. The fields of the proxy are not initialized, so they will have a default value ({@code null},
+     * {@code 0}, {@code false}).</li>
+     * <li>Neither should the class be final or have any final methods, because the proxy won't be able to override
+     * them otherwise. A final class will produce an exception when trying to create a proxy, but final fields may be
+     * ignored silenty and produce unspecified behaviour.</li>
+     * <li>The constructor of the target class will not be called when the proxy (which is its subclass) is created,
+     * so the class does not need to have an accessible default constructor.</li>
+     * </ul>
      */
     CLASS
 }
