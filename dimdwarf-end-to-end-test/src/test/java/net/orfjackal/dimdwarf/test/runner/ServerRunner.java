59c1b374b2397725eaf5fb703bd10939e40c0636
Esko Luontola <esko.luontola@gmail.com>
2010-06-10 20:02:54 +0300

    End-to-end tests: launch server process

--- dimdwarf-end-to-end-test/src/test/java/net/orfjackal/dimdwarf/test/runner/ServerRunner.java
+++ dimdwarf-end-to-end-test/src/test/java/net/orfjackal/dimdwarf/test/runner/ServerRunner.java
@@ -4,22 +4,53 @@
 
 package net.orfjackal.dimdwarf.test.runner;
 
+import net.orfjackal.dimdwarf.test.util.TestEnvironment;
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
 import java.util.Random;
 
 public class ServerRunner {
 
     private final String host = "localhost";
     private final int port;
+    private Process serverProcess;
 
     public ServerRunner() {
         port = new Random().nextInt(10000) + 10000;
     }
 
-    public void startApplication(Class<?> application) {
+    public void startApplication(Class<?> application) throws IOException {
+        ProcessBuilder builder = new ProcessBuilder();
+        builder.directory(TestEnvironment.getDeploymentDir());
+        builder.redirectErrorStream(false);
+        builder.command("java", "-jar", "launcher.jar");
+
+        serverProcess = builder.start();
+        redirectStream(serverProcess.getInputStream(), System.out);
+        redirectStream(serverProcess.getErrorStream(), System.err);
+    }
+
+    private static void redirectStream(final InputStream in, final OutputStream out) {
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    IOUtils.copy(in, out);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.setDaemon(true);
+        t.start();
     }
 
     public void shutdown() {
         try {
+            if (serverProcess != null) {
+                serverProcess.destroy();
+                serverProcess.waitFor();
+            }
         } catch (Throwable t) {
             t.printStackTrace();
         }
