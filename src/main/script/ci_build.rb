135c5f645a569f9a89826c1dde66b17b6b48f873
Esko Luontola <esko.luontola@gmail.com>
2010-06-12 21:39:13 +0300

    Documentation for CI build script

--- src/main/script/ci_build.rb
+++ src/main/script/ci_build.rb
@@ -1,6 +1,27 @@
 #!/usr/bin/ruby
 require File.dirname($0) + '/common'
 
+# Does a clean build on the project and pushes the changes to the
+# remote Git repository if there are no failures. Otherwise reverts the
+# local repository to the version at the remote repository.
+#
+# The workflow supported by this script is that the office has one
+# workstation dedicated for manual CI builds. The team members have
+# only read access to the central repository, but the CI machine has
+# also write access. When a team member wants to push his changes to
+# the server, he will first push them to the CI machine. Then he will
+# walk to the CI machine and run this script (you could even automate
+# this step with a 'post-receive' hook).
+#
+# If the script succeeds, the change is pushed to the central
+# repository and from there others will get the changes (a sound signal
+# at this point could be useful). If the change fails, the CI machine
+# automatically reverts all changes (so that someone else can come use
+# the CI machine) and the team member will walk back to his own
+# workstation to fix the bug (after inspecting the build logs on the CI
+# machine). This way the code on the central repository is guaranteed
+# to always pass the build.
+
 BRANCH = 'master'
 REMOTE = 'origin'
 
