9aa7e0d9ca47151b0b0ef410018ff93aff2ad660
Esko Luontola <esko.luontola@gmail.com>
2010-06-15 02:19:35 +0300

    Made the agent module's artifact have no classifier
    Made the agent module a pure aggregator by moving the agent class to the aop module

--- dimdwarf-aop/src/main/java/net/orfjackal/dimdwarf/aop/AopAgent.java
+++ dimdwarf-aop/src/main/java/net/orfjackal/dimdwarf/aop/AopAgent.java
@@ -2,7 +2,7 @@
 // This software is released under the Apache License 2.0.
 // The license text is at http://dimdwarf.sourceforge.net/LICENSE
 
-package net.orfjackal.dimdwarf.agent;
+package net.orfjackal.dimdwarf.aop;
 
 import net.orfjackal.dimdwarf.aop.conf.*;
 
