c796a0eefff506ae3c61ec04d41e3cb4357ae702
Esko Luontola <esko.luontola@gmail.com>
2010-06-15 23:11:00 +0300

    SimpleSgsProtocolDecoder: read the message's payload length

--- dimdwarf-core/src/main/java/net/orfjackal/dimdwarf/net/SimpleSgsProtocolDecoder.java
+++ dimdwarf-core/src/main/java/net/orfjackal/dimdwarf/net/SimpleSgsProtocolDecoder.java
@@ -4,6 +4,7 @@
 
 package net.orfjackal.dimdwarf.net;
 
+import com.sun.sgs.protocol.simple.SimpleSgsProtocol;
 import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.session.IoSession;
 import org.apache.mina.filter.codec.*;
@@ -16,13 +17,20 @@ public class SimpleSgsProtocolDecoder extends CumulativeProtocolDecoder {
     // TODO: ensure compatibility with com.sun.sgs.impl.sharedutil.MessageBuffer
 
     protected boolean doDecode(IoSession session, IoBuffer in, ProtocolDecoderOutput out) throws Exception {
+        if (in.prefixedDataAvailable(2, SimpleSgsProtocol.MAX_PAYLOAD_LENGTH)) {
+            int length = in.getUnsignedShort();
+            byte op = in.get();
+            assert op != SimpleSgsProtocol.LOGIN_REQUEST;
 
-        // "doDecode() can't return true when buffer is not consumed."
-        for (int i = 0; i < in.limit(); i++) {
-            in.get();
+            for (int i = 1; i < length; i++) {
+                in.get();
+            }
+            LoginRequest message = new LoginRequest();
+
+            out.write(message);
+            return true;
+        } else {
+            return false;
         }
-
-        out.write(new LoginRequest());
-        return true;
     }
 }
