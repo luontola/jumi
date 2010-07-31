8cdf0c66b5aa23b6402e071ea494dd8b939f2eb1
Esko Luontola <esko.luontola@gmail.com>
2010-08-01 00:20:35 +0300

    Begin using Scala in the core module

---
+++ dimdwarf-core/src/test/scala/net/orfjackal/dimdwarf/net/SimpleSgsProtocolSpec.scala
@@ -0,0 +1,12 @@
+package net.orfjackal.dimdwarf.net
+
+import org.junit.Assert._
+import org.hamcrest.CoreMatchers._
+import org.junit.runner.RunWith
+import net.orfjackal.specsy.Specsy
+import net.orfjackal.specsy.junit.SpecsyJUnitRunner
+
+@RunWith(classOf[SpecsyJUnitRunner])
+class SimpleSgsProtocolSpec extends Specsy {
+  assertThat(true, is(true)) // TODO
+}
