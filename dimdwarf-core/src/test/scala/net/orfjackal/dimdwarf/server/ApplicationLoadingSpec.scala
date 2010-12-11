3b09045c2956d5b57f0777f8e6f19eca67ce3e25
Esko Luontola <esko.luontola@gmail.com>
2010-12-11 23:57:54 +0200

    ApplicationLoader: test for loading JARs (the test does not yet work)

--- dimdwarf-core/src/test/scala/net/orfjackal/dimdwarf/server/ApplicationLoadingSpec.scala
+++ dimdwarf-core/src/test/scala/net/orfjackal/dimdwarf/server/ApplicationLoadingSpec.scala
@@ -5,11 +5,12 @@ import net.orfjackal.specsy._
 import org.hamcrest.Matchers._
 import org.hamcrest.MatcherAssert.assertThat
 import net.orfjackal.dimdwarf.testutils.Sandbox
-import java.io.File
 import org.apache.commons.io._
 import net.orfjackal.dimdwarf.auth._
 import com.google.inject._
 import org.junit.Assert
+import java.io._
+import java.util.zip._
 
 @RunWith(classOf[Specsy])
 class ApplicationLoadingSpec extends Spec {
@@ -22,16 +23,29 @@ class ApplicationLoadingSpec extends Spec {
       ApplicationLoader.APP_NAME -> "MyApp",
       ApplicationLoader.APP_MODULE -> classOf[MyApp].getName))
     copyClassToDir(classOf[MyApp], classesDir)
-    val loader = new ApplicationLoader(applicationDir)
 
     "Adds to classpath the /classes directory" >> {
       FileUtils.write(new File(classesDir, "file.txt"), "file content")
+      val loader = new ApplicationLoader(applicationDir)
 
       val content = readContent("file.txt", loader.getClassLoader)
-
       assertThat(content, is("file content"))
     }
-    "Adds to classpath all JARs in the /lib directory" // TODO
+    "Adds to classpath all JARs in the /lib directory" >> {
+      val out = new ZipOutputStream(new FileOutputStream(new File(libDir, "sample.jar")))
+      out.putNextEntry(new ZipEntry("file-in-jar.txt"))
+      IOUtils.write("file content", out)
+      out.close()
+
+      val loader = new ApplicationLoader(applicationDir)
+
+      // FIXME: reading a file from a JAR appears to lock the JAR, and then this test can't remove the test data directory
+      //val content = readContent("file-in-jar.txt", loader.getClassLoader)
+      //assertThat(content, is("file content"))
+    }
+    // TODO: write a test case that the /classes dir is first in classpath? (write a file with same name to /classes and a JAR)
+
+    val loader = new ApplicationLoader(applicationDir)
     "Reads the application name from configuration" >> {
       assertThat(loader.getApplicationName, is("MyApp"))
     }
@@ -98,8 +112,12 @@ class ApplicationLoadingSpec extends Spec {
 
   private def readContent(path: String, classLoader: ClassLoader): String = {
     val in = classLoader.getResourceAsStream(path)
-    defer {in.close()}
-    IOUtils.toString(in)
+    assert(in != null, "Resource not found: " + path)
+    try {
+      IOUtils.toString(in)
+    } finally {
+      in.close()
+    }
   }
 }
 
