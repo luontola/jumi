d9c38ede366554a4f22e7af6d406e547285a6937
Esko Luontola <esko.luontola@gmail.com>
2013-02-06 23:47:04 +0200

    Failing end-to-end test for ignoring abstract classes

---
+++ end-to-end-tests/src/test/java/fi/jumi/test/IgnoringTestsTest.java
@@ -0,0 +1,24 @@
+// Copyright © 2011-2013, Esko Luontola <www.orfjackal.net>
+// This software is released under the Apache License 2.0.
+// The license text is at http://www.apache.org/licenses/LICENSE-2.0
+
+package fi.jumi.test;
+
+import org.junit.*;
+import sample.AbstractTest;
+
+public class IgnoringTestsTest {
+
+    @Rule
+    public final AppRunner app = new AppRunner();
+
+
+    @Ignore("not implemented") // TODO
+    @Test(timeout = Timeouts.END_TO_END_TEST)
+    public void ignores_abstract_test_classes_silently() throws Exception {
+        app.runTests(AbstractTest.class);
+
+        app.checkPassingAndFailingTests(0, 0);
+        app.checkTotalTestRuns(0);
+    }
+}
