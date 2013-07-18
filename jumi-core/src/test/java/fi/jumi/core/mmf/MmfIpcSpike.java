6ead19c26a09cefba89ab1b03d646210251f1151
Esko Luontola <esko.luontola@gmail.com>
2013-07-18 13:46:00 +0300

    Added IPC measurements on Lynnfield, also using AtomicBoolean

--- jumi-core/src/test/java/fi/jumi/core/mmf/MmfIpcSpike.java
+++ jumi-core/src/test/java/fi/jumi/core/mmf/MmfIpcSpike.java
@@ -7,6 +7,7 @@ package fi.jumi.core.mmf;
 import java.io.*;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -20,9 +21,12 @@ public class MmfIpcSpike {
 
     private static final int FILE_SIZE = 1024 * 1024;
     private static final int TAG_INDEX = 0;
-    private static final int DATA_INDEX = FILE_SIZE - 4;
+    private static final int DATA_INDEX = 1;                // same cache line
+//    private static final int DATA_INDEX = FILE_SIZE - 4;  // different cache line
 
-    public static volatile boolean memoryBarrier;
+    // ways of creating a memory barrier
+    public static volatile boolean volatileField;
+    public static final AtomicBoolean atomicBoolean = new AtomicBoolean();
 
     private static MappedByteBuffer openMemoryMappedFile() throws IOException {
         RandomAccessFile file = new RandomAccessFile("MmfIpcSpike.tmp", "rw");
@@ -31,12 +35,34 @@ public class MmfIpcSpike {
     }
 
     private static void loopWait() throws Exception {
-        // Measured on Intel Core 2 Quad Q6600 (Kentsfield), 2.4GHz @ 3.0GHz
-        memoryBarrier = true;       // 125.55 ns IPC latency (two cache lines), 99.9 ns IPC latency (one cache line)
-//        Thread.yield();           // 208.25 ns IPC latency
-//        LockSupport.parkNanos(1); // 695000.0 ns IPC latency
-//        Thread.sleep(1);          // 704000.0 ns IPC latency
-//        ;                         // deadlocks, maybe because of a compiler optimization?
+        atomicBoolean.lazySet(true);
+//        atomicBoolean.set(true);
+//        volatileField = true;
+//        Thread.yield();
+//        LockSupport.parkNanos(1);
+//        Thread.sleep(1);
+
+        // ## IPC latency results ##
+        //
+        // Intel Core 2 Quad Q6600 @ 3.0GHz (Kentsfield), Windows 7 64-bit
+        //              1 cache line    2 cache lines
+        // lazySet()
+        // set()
+        // volatile     99.9 ns         125.55 ns
+        // yield()                      208.25 ns
+        // parkNanos(1)                 695000.0 ns
+        // sleep(1)                     704000.0 ns
+        // none         deadlock        deadlock
+        //
+        // Intel Core i7 860 @ 2.8GHz (Lynnfield), Windows 7 64-bit, jdk1.7.0_17 64-bit
+        //              1 cache line    2 cache lines
+        // lazySet()    68.85 ns        91.9 ns
+        // set()        47.7 ns         95.0 ns
+        // volatile     47.65-78.6 ns   91.65 ns
+        // yield()      74.1 ns         110.3 ns
+        // parkNanos(1) 640900.0 ns     691700.0 ns
+        // sleep(1)     711150.0 ns     655000.0 ns
+        // none         deadlock        deadlock
     }
 
     public static class Writer {
