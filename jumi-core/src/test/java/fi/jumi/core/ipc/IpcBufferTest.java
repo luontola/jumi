ee3fd69feac8211146bfe6ffae357c01eca78761
Esko Luontola <esko.luontola@gmail.com>
2013-08-13 21:25:05 +0300

    TestableRandom for randomized testing

--- jumi-core/src/test/java/fi/jumi/core/ipc/IpcBufferTest.java
+++ jumi-core/src/test/java/fi/jumi/core/ipc/IpcBufferTest.java
@@ -4,32 +4,41 @@
 
 package fi.jumi.core.ipc;
 
-import org.junit.Test;
+import org.junit.*;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class IpcBufferTest {
 
+    @Rule
+    public final TestableRandom random = new TestableRandom();
+
     @Test
     public void absolute_byte() {
-        IpcBuffer buffer = new IpcBuffer()
-                .setByte(0, (byte) 40)
-                .setByte(1, (byte) 50);
+        byte b1 = random.nextByte();
+        byte b2 = random.nextByte();
 
-        assertThat(buffer.getByte(0), is((byte) 40));
-        assertThat(buffer.getByte(1), is((byte) 50));
+        IpcBuffer buffer = new IpcBuffer()
+                .setByte(0, b1)
+                .setByte(1, b2);
+
+        assertThat(buffer.getByte(0), is(b1));
+        assertThat(buffer.getByte(1), is(b2));
     }
 
     @Test
     public void relative_byte() {
+        byte b1 = random.nextByte();
+        byte b2 = random.nextByte();
+
         IpcBuffer buffer = new IpcBuffer()
-                .writeByte((byte) 40)
-                .writeByte((byte) 50)
+                .writeByte(b1)
+                .writeByte(b2)
                 .position(0);
 
-        assertThat(buffer.readByte(), is((byte) 40));
-        assertThat(buffer.readByte(), is((byte) 50));
+        assertThat(buffer.readByte(), is(b1));
+        assertThat(buffer.readByte(), is(b2));
     }
 
     // TODO: generic scaffolding for random data; on failure show the seed that was used
---
+++ jumi-core/src/test/java/fi/jumi/core/ipc/TestableRandom.java
@@ -0,0 +1,64 @@
+// Copyright © 2011-2013, Esko Luontola <www.orfjackal.net>
+// This software is released under the Apache License 2.0.
+// The license text is at http://www.apache.org/licenses/LICENSE-2.0
+
+package fi.jumi.core.ipc;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+import java.util.Random;
+
+public class TestableRandom implements TestRule {
+
+    private final long seed;
+    private final Random random;
+
+    public TestableRandom() {
+        this(System.currentTimeMillis());
+    }
+
+    public TestableRandom(long seed) {
+        this.seed = seed;
+        this.random = new Random(seed);
+    }
+
+    public byte nextByte() {
+        return (byte) random.nextInt();
+    }
+
+    public int nextInt() {
+        return random.nextInt();
+    }
+
+    @Override
+    public Statement apply(Statement base, Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                try {
+                    base.evaluate();
+                } catch (Throwable t) {
+                    throw new ExtraMessageAssertionError(TestableRandom.class.getSimpleName() + " seed was " + seed + "L", t);
+                }
+            }
+        };
+    }
+
+    private static class ExtraMessageAssertionError extends AssertionError {
+        private final String extraMessage;
+        private final Throwable realException;
+
+        public ExtraMessageAssertionError(String extraMessage, Throwable realException) {
+            this.extraMessage = extraMessage;
+            this.realException = realException;
+            this.setStackTrace(realException.getStackTrace());
+        }
+
+        @Override
+        public String toString() {
+            return extraMessage + "\n" + realException.toString();
+        }
+    }
+}
