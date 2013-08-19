ac080bea23dbbe6ffec91abf0f3ea4b771cbd153
Esko Luontola <esko.luontola@gmail.com>
2013-08-19 18:56:34 +0300

    Improved resilience for two processes opening the file concurrently

--- jumi-core/src/main/java/fi/jumi/core/ipc/MappedByteBufferSequence.java
+++ jumi-core/src/main/java/fi/jumi/core/ipc/MappedByteBufferSequence.java
@@ -35,10 +35,16 @@ public class MappedByteBufferSequence implements ByteBufferSequence {
     }
 
     private static MappedByteBuffer tryMapFile(Path path, long size) throws IOException {
+        OpenOption[] options;
         if (Files.exists(path)) {
             size = Files.size(path);
+            options = new OpenOption[]{READ, WRITE};
+        } else {
+            // To avoid a race condition if two processes open the file concurrently,
+            // we use here CREATE_NEW instead of CREATE
+            options = new OpenOption[]{READ, WRITE, CREATE_NEW};
         }
-        try (FileChannel fc = FileChannel.open(path, READ, WRITE, CREATE)) {
+        try (FileChannel fc = FileChannel.open(path, options)) {
             return fc.map(FileChannel.MapMode.READ_WRITE, 0, size);
         }
     }
