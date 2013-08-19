492a9d3960fc763487f67a496a0ed580706b5c4e
Esko Luontola <esko.luontola@gmail.com>
2013-08-19 19:43:54 +0300

    Failing test for SuiteEventSerializer

---
+++ jumi-core/src/main/java/fi/jumi/core/serialization/SuiteEventSerializer.java
@@ -0,0 +1,77 @@
+// Copyright © 2011-2013, Esko Luontola <www.orfjackal.net>
+// This software is released under the Apache License 2.0.
+// The license text is at http://www.apache.org/licenses/LICENSE-2.0
+
+package fi.jumi.core.serialization;
+
+import fi.jumi.api.drivers.TestId;
+import fi.jumi.core.api.*;
+import fi.jumi.core.ipc.IpcBuffer;
+
+import javax.annotation.concurrent.NotThreadSafe;
+
+@NotThreadSafe
+public class SuiteEventSerializer implements SuiteListener {
+
+    public static void deserialize(IpcBuffer source, SuiteListener target) {
+    }
+
+    public SuiteEventSerializer(IpcBuffer buffer) {
+    }
+
+    @Override
+    public void onSuiteStarted() {
+    }
+
+    @Override
+    public void onInternalError(String message, StackTrace cause) {
+    }
+
+    @Override
+    public void onTestFileFound(TestFile testFile) {
+    }
+
+    @Override
+    public void onAllTestFilesFound() {
+    }
+
+    @Override
+    public void onTestFound(TestFile testFile, TestId testId, String name) {
+    }
+
+    @Override
+    public void onRunStarted(RunId runId, TestFile testFile) {
+    }
+
+    @Override
+    public void onTestStarted(RunId runId, TestId testId) {
+    }
+
+    @Override
+    public void onPrintedOut(RunId runId, String text) {
+    }
+
+    @Override
+    public void onPrintedErr(RunId runId, String text) {
+    }
+
+    @Override
+    public void onFailure(RunId runId, StackTrace cause) {
+    }
+
+    @Override
+    public void onTestFinished(RunId runId) {
+    }
+
+    @Override
+    public void onRunFinished(RunId runId) {
+    }
+
+    @Override
+    public void onTestFileFinished(TestFile testFile) {
+    }
+
+    @Override
+    public void onSuiteFinished() {
+    }
+}
---
+++ jumi-core/src/test/java/fi/jumi/core/serialization/SuiteEventSerializerTest.java
@@ -0,0 +1,74 @@
+// Copyright © 2011-2013, Esko Luontola <www.orfjackal.net>
+// This software is released under the Apache License 2.0.
+// The license text is at http://www.apache.org/licenses/LICENSE-2.0
+
+package fi.jumi.core.serialization;
+
+import fi.jumi.api.drivers.TestId;
+import fi.jumi.core.api.*;
+import fi.jumi.core.ipc.*;
+import fi.jumi.core.util.SpyListener;
+import org.junit.*;
+
+import java.lang.reflect.Method;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+
+public class SuiteEventSerializerTest {
+
+    @Ignore // TODO
+    @Test
+    public void serializes_all_events() {
+        SpyListener<SuiteListener> spy = new SpyListener<>(SuiteListener.class);
+        exampleUsage(spy.getListener());
+        spy.replay();
+        IpcBuffer buffer = new IpcBuffer(new AllocatedByteBufferSequence(1024));
+
+        // serialize
+        exampleUsage(new SuiteEventSerializer(buffer));
+
+        // deserialize
+        buffer.position(0);
+        SuiteEventSerializer.deserialize(buffer, spy.getListener());
+
+        spy.verify();
+    }
+
+    @Test
+    public void example_usage_invokes_every_method_in_the_interface() {
+        SuiteListenerSpy spy = new SuiteListenerSpy();
+
+        exampleUsage(spy);
+
+        for (Method method : SuiteListener.class.getMethods()) {
+            assertThat("invoked methods", spy.methodInvocations.keySet(), hasItem(method));
+        }
+    }
+
+    private static void exampleUsage(SuiteListener listener) {
+        TestFile testFile = TestFile.fromClassName("com.example.SampleTest");
+        RunId runId = new RunId(1);
+
+        listener.onSuiteStarted();
+        listener.onTestFileFound(testFile);
+        listener.onAllTestFilesFound();
+
+        listener.onTestFound(testFile, TestId.ROOT, "SampleTest");
+        listener.onTestFound(testFile, TestId.of(0), "testName");
+
+        listener.onRunStarted(runId, testFile);
+        listener.onTestStarted(runId, TestId.ROOT);
+        listener.onTestStarted(runId, TestId.of(0));
+        listener.onPrintedOut(runId, "printed to out");
+        listener.onPrintedErr(runId, "printed to err");
+        listener.onFailure(runId, StackTrace.copyOf(new AssertionError("assertion message")));
+        listener.onTestFinished(runId);
+        listener.onTestFinished(runId);
+        listener.onRunFinished(runId);
+
+        listener.onInternalError("error message", StackTrace.copyOf(new Exception("exception message")));
+        listener.onTestFileFinished(testFile);
+        listener.onSuiteFinished();
+    }
+}
