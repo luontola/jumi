2dc677ee8ae76c12a5f606fd8b34adb3aa42fb24
Esko Luontola <esko.luontola@gmail.com>
2013-08-26 21:11:26 +0300

    Links for more information about memory barriers

--- jumi-core/src/main/java/fi/jumi/core/util/MemoryBarrier.java
+++ jumi-core/src/main/java/fi/jumi/core/util/MemoryBarrier.java
@@ -7,6 +7,15 @@ package fi.jumi.core.util;
 import javax.annotation.concurrent.ThreadSafe;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+
+/**
+ * Operations for creating memory barriers. More information:
+ * <ul>
+ *     <li><a href="http://g.oswego.edu/dl/jmm/cookbook.html">The JSR-133 Cookbook for Compiler Writers</a></li>
+ *     <li><a href="http://psy-lob-saw.blogspot.com/2012/12/atomiclazyset-is-performance-win-for.html">Atomic*.lazySet is a performance win for single writers</a></li>
+ *     <li><a href="http://brooker.co.za/blog/2012/09/10/volatile.html">Are volatile reads really free?</a></li>
+ * </ul>
+ */
 @ThreadSafe
 public class MemoryBarrier {
 
@@ -20,7 +29,7 @@ public class MemoryBarrier {
         var.set(true);
     }
 
-    public void read() {
+    public void loadLoad() {
         var.get();
     }
 }
