1f20919d8ed756492e215892345125b75811b9dd
Esko Luontola <esko.luontola@gmail.com>
2014-07-13 21:24:17 +0300

    Created core.ipc.dirs package

--- jumi-core/src/test/java/fi/jumi/core/ipc/dirs/DirectoryObserverTest.java
+++ jumi-core/src/test/java/fi/jumi/core/ipc/dirs/DirectoryObserverTest.java
@@ -2,9 +2,10 @@
 // This software is released under the Apache License 2.0.
 // The license text is at http://www.apache.org/licenses/LICENSE-2.0
 
-package fi.jumi.core.ipc;
+package fi.jumi.core.ipc.dirs;
 
 import fi.jumi.core.Timeouts;
+import fi.jumi.core.ipc.*;
 import fi.jumi.core.util.TestingExecutor;
 import org.apache.commons.io.FileUtils;
 import org.hamcrest.Matcher;
